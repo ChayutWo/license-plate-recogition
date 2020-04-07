77a07092e11a895eed8d46fbe19599c25cd16224
Yingyu <yingyu.fu@outlook.com>
2020-04-06 20:48:58 -0400

    format

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -2,12 +2,13 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
-#from scipy.io import loadmat
+# from scipy.io import loadmat
 import numpy as np
 from skimage import io, transform
 import os
 import os.path
 from utils.variables import *
+
 
 # Dataloader class
 class LicenseLandmarksDataset(Dataset):
@@ -29,32 +30,32 @@ class LicenseLandmarksDataset(Dataset):
         return len(self.landmarks_frame)
 
     def four_corner(self, landmarks):
-        x=landmarks[0]
-        y=landmarks[1]
-        width= landmarks[2]
-        height= landmarks[3]
-        corner = [x,y,x+width,y,x,y+height,x+width,y+height]
+        x = landmarks[0]
+        y = landmarks[1]
+        width = landmarks[2]
+        height = landmarks[3]
+        corner = [x, y, x + width, y, x, y + height, x + width, y + height]
         corner = np.array(corner)
         corner = corner.reshape(-1, 2)
         return corner
 
-    def get_box (self, landmarks):
-        x_max = np.max(landmarks[:,0])
-        y_max = np.max(landmarks[:,1])
-        x_min = np.min(landmarks[:,0])
-        y_min = np.min(landmarks[:,1])
+    def get_box(self, landmarks):
+        x_max = np.max(landmarks[:, 0])
+        y_max = np.max(landmarks[:, 1])
+        x_min = np.min(landmarks[:, 0])
+        y_min = np.min(landmarks[:, 1])
         width = x_max - x_min
         height = y_max - y_min
-        return [x_min, y_min, width,height]
+        return [x_min, y_min, width, height]
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        img_name = self.root_dir+self.landmarks_frame.iloc[idx, 2]
+        img_name = self.root_dir + self.landmarks_frame.iloc[idx, 2]
         image = io.imread(img_name)
         landmarks = self.landmarks_frame.iloc[idx, 10]
-        landmarks= landmarks.strip("][").replace("'",'')\
-        .replace('"','').split(', ')
+        landmarks = landmarks.strip("][").replace("'", '') \
+            .replace('"', '').split(', ')
         landmarks = np.array(landmarks)
         landmarks = landmarks.astype('float')
         landmarks = self.four_corner(landmarks)
